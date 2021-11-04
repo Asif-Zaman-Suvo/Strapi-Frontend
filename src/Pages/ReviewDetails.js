56d213df6f3837aa4a508868c70697bab7958387
Md Asifuzzaman Suvo <asifshuvo119181@gmail.com>
2021-11-04 18:57:19 +0600

    lesson 7 done

--- src/Pages/ReviewDetails.js
+++ src/Pages/ReviewDetails.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router";
+import useFetch from "../Hooks/useFetch";
 
 const ReviewDetails = () => {
-    return (
-        <div>
+  const { id } = useParams();
+  const { loading, error, data } = useFetch(
+    'http://localhost:1337/reviews/' + id
+  )
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error:(</p>;
 
-            Review Details
-            
-        </div>
-    );
+  console.log(data);
+
+  return (
+    <div className="review-card">
+      <div className="rating">{data.rating}</div>
+      <h2>{data.title}</h2>
+
+      <small> console list</small>
+
+      <p>{data.body}....</p>
+    </div>
+  );
 };
 
 export default ReviewDetails;
